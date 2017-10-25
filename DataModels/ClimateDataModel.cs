bf6d9e612ec69807004007d8ffe43991d7445890
Al Doherty <al.doherty73@gmail.com>
2017-10-25 15:15:46 +1000

    test

--- DataModels/ClimateDataModel.cs
+++ DataModels/ClimateDataModel.cs
@@ -8,10 +8,10 @@ using System.Collections.Generic;
 namespace HowLeaky.DataModels
 {
     public class ClimateDataModel : DataModel
-    {//
-        public String StationCode { get; set; }//
+    {
+        public String StationCode { get; set; }
 
-        public string Country { get; set; }//{}//
+        public string Country { get; set; }
         public String State { get; set; }
 
         public float Latitude { get; set; }
